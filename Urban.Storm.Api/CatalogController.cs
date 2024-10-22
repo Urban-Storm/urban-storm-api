86cfcc4eff068a1ac57afb05b9096811ea2e79cd
Amie Lehman <Lehman.497@osu.edu>
2024-10-22 18:33:58 -0400

    Swagger Commit

--- Urban.Storm.Api/CatalogController.cs
+++ Urban.Storm.Api/CatalogController.cs
@@ -10,7 +10,12 @@ namespace Urban.Storm.Api.Controllers
         [HttpGet]
         public IActionResult GetItems()
         {
-            return Ok("hello world.");
+           
+        var items = new List<Item>(){
+            new Item("Shirt", "Ohio State shirt.", "Nike", 29.99m),
+            new Item("Shorts", "Ohio State shorts.", "Nike", 44.99m),
+        };
+        return Ok(items);
         }
     }
 }
