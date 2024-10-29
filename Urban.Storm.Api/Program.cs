bdcbd22e169740d507037f2a1fbf375d1dd83626
Rachel Max <racheljmax@gmail.com>
2024-10-29 11:33:46 -0400

    migrations commit

--- Urban.Storm.Api/Program.cs
+++ Urban.Storm.Api/Program.cs
@@ -1,18 +1,31 @@
 using Urban.Storm.Data;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Sqlite;
 using Microsoft.Extensions.Options;
 using Microsoft.AspNetCore.Mvc.ModelBinding.Binders;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Add services to the container.
 builder.Services.AddControllers();
 builder.Services.AddDbContext<StoreContext>(options =>
-    options.UseSqlite("Data Source=../Registrar.sqlite", 
-    b => b.MigrationsAssembly("Urban.Storm.Api"))); 
-    
-    
-    builder.Services.AddEndpointsApiExplorer();
-    builder.Services.AddSwaggerGen(); 
+    options.UseSqlite("Data Source=../Registrar.sqlite",
+    b => b.MigrationsAssembly("Urban.Storm.Api"))
+);
 
-  
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
