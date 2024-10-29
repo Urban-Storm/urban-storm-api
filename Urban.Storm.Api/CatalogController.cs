48674593bcd60a083ee81f8ec2af925836171d5c
Rachel Max <racheljmax@gmail.com>
2024-10-29 11:59:36 -0400

    PUT and Delete commit

--- Urban.Storm.Api/CatalogController.cs
+++ Urban.Storm.Api/CatalogController.cs
@@ -43,18 +43,38 @@ namespace Urban.Storm.Api.Controllers
    }
    [HttpPost("{id:int}/ratings")]
    public IActionResult PostRating(int id, [FromBody] Rating rating) {
-    var item = new Item ("Shirt", "Ohio State shirt.", "Nike", 29.99m);
-    item.Id = id;
-    item.AddRating(rating);
-    return Ok(item); 
+            var item = _db.Items.Find(id);
+            if (item == null) {
+                return NotFound();
+            }
+            item.AddRating(rating);
+            _db.SaveChanges();
+
+            return Ok(item);
    }
    [HttpPut("{id:int}")]
    public IActionResult Put(int id, Item item){
-    return NoContent();
+    if (id != item.Id) {
+                return BadRequest();
+    }
+    if (_db.Items.Find(id) == null) {
+                return NotFound();
+    }
+            _db.Entry(item).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+            _db.SaveChanges();
+
+            return NoContent();
    }
    [HttpDelete("{id:int}")]
 public IActionResult Delete(int id){
-    return NoContent();
+            var item = _db.Items.Find(id);
+            if (item == null) {
+                return NotFound();
+            }
+            _db.Items.Remove(item);
+            _db.SaveChanges();
+
+            return Ok();
 }
     }
 }
